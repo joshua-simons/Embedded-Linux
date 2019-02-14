5cb767d2c06a2fbc109564c26998e6670dd8fbaf
Josh Simons <simonsj@newpaltz.edu>
2019-02-14 10:15:23 -0500

    changed blink.py to have a switch trigger the LED

--- python/blink.py
+++ python/blink.py
@@ -1,21 +1,34 @@
-#Import libraries
-import RPi.GPIO as GPIO
+# Import the necessary libraries to work the GPIO
+import RPi.GPIO as GPIO 
 import time
+import signal
+import os
 
-#Initialize the GPIO
-GPIO.setmode(GPIO.BCM)
+# Initialize the GPIO
+GPIO.setmode (GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#This function will make the light blink once
+# Define the function that will toggle the state of the light
 def blinkOnce(pin):
 	GPIO.output(pin,True)
-	time.sleep(1)
+	time.sleep(.1)
 	GPIO.output(pin,False)
-	time.sleep(1)
+	time.sleep(.1)
+	
 
-#Call the blinkOnce function above in a loop
-for i in range(10):
-	blinkOnce(17)
+# Use the blinkonce function in a loopity-loop when the button is pressed
+try:
+	while True:
+		input_state = GPIO.input(26)
+		if input_state == False:
+			for i in range (15):
+				blinkOnce(17)
+			time.sleep(0.2)
+			
+	
+except KeyboardInterrupt:
+	os.system('clear')
+	print('Thanks for Blinking and Thinking!')
+	GPIO.cleanup()
 
-#Cleanup the GPIO when done
-GPIO.cleanup()
